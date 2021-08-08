a00584997701866854110150ef94db6da1c58f3a
JeromeBenedetti <benedetti.jerome@laposte.net>
2021-08-08 12:02:36 +0200

    feat(admin): redirect admin index to conference

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,14 +9,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Conference;
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
         
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
