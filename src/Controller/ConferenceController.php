20d2f8ed419e640066dd8e69367ab38131059104
JeromeBenedetti <benedetti.jerome@laposte.net>
2021-08-10 15:34:37 +0200

    feat(Conference): add comment pagination

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -29,9 +29,13 @@ class ConferenceController extends AbstractController
     #[Route('/conference/{id}', name: 'conference')]
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
